ec21a8e2bffb5324a981f84dbc2c2ece304ac4e5
boomiha <boomiha08@gmail.com>
2024-12-29 20:05:47 +0530

    find min in rotated sorted array

---
+++ FindMinimumInRotatedSortedarray/FindMinimumInRotatedSortedArray.c
@@ -0,0 +1,10 @@
+int findMin(int* nums, int numsSize) 
+{
+      for (int i = 1; i < numsSize; i++) {
+        if (nums[i] < nums[i - 1]) {
+            return nums[i];  
+        }
+    }
+    return nums[0];
+   
+}
